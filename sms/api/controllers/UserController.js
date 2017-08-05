ab656219b942662b7172e913e678734bd3310634
tiaksim <tilak.ruthva@gmail.com>
2017-08-06 05:12:36 +0530

    Login and create finished.

--- sms/api/controllers/UserController.js
+++ sms/api/controllers/UserController.js
@@ -1,52 +1,124 @@
 /**
- * UserController
- *
- * @description :: Server-side logic for managing Users
- * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
- */
-'use strict';
- module.exports = {
-     create(req, res) {
-         const data = req.body;
-         if (data.password !== data.confirmPassword) return res.badRequest("Password not the same");
+* UserController
+*
+* @description :: Server-side logic for managing users
+* @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+*/
 
-         User.create({
-                 email: data.email,
-                 password: data.password,
-                 name: data.name
-                     //etc...
-             })
-             .then((user) => {
-                 res.send({ token: jwToken.issue({ id: user.id }) }); // payload is { id: user.id}
-             })
-             .catch((err) => {
-                 sails.log.error(err);
-                 return res.serverError("Something went wrong");
-             });
-     },
+module.exports = {
 
-     login(req, res) {
-         const data = req.body;
 
-         if (!data.email || !data.password) return res.badRequest('Email and password required');
 
-         User.findOne({ email: data.email })
-             .then((user) => {
-				 console.log(user);
-                 if (!user) return res.notFound();
+  /**
+  * `UserController.login()`
+  */
+  create(req, res) {
+    const data = req.body;
+    if (data.paasword != data.confirmPassowrd) return res.badRequest("Password not the same");
+    if (!data.ord_id && !data.inst_id){
+      res.status('406');
+      return res.json({
+        status: -406,
+        message: "partial content!!"
+      });
+    }
+    // User.beforeCreate(data,function(err){});
 
-                 User.comparePassword(data.password, user.encryptedPassword)
-                     .then(() => {
-                         return res.send({ token: jwToken.issue({ id: user.id }) })
-                     })
-                     .catch((err) => {
-						 console.log('forbidden');
-                         return res.forbidden();
-                     });
-             })
-             .catch((err) => {
-                 sails.log.error(err);
-                 return res.serverError();
-             });
-     }
- };
+    User.findOne({
+      or : [
+        { username: data.username },
+        { email: data.email }
+      ]
+    })
+    .then((user) => {
+      if(user){
+        res.status('403');
+        return res.json({
+          status: -403,
+          message: "User already exists!!"
+        });
+      }else{
+        User.create(data)
+        .then((user) => {
+          res.status('201');
+          res.json({
+            status: 201,
+            message: "User created!!"
+          });
+        })
+        .catch((err) => {
+          sails.log.error(err);
+          return res.serverError("Something went wrong");
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+
+  },
+  login: function (req, res) {
+    const data = req.body;
+    if (data.username && data.password && data.loginas) {
+      var username = data.username;
+      var password = data.password;
+      var loginas = data.loginas;
+      var rand = function() {
+        return Math.random().toString(36).substr(2); // remove `0.`
+      };
+      var token = function() {
+        return rand() + rand(); // to make it longer
+      };
+      var tokenGen = null;
+      User.findOne({
+        or:[
+          { username: username},
+          { email: username}
+        ]
+      }).exec(function (err, user) {
+        if (err){
+          return res.negotiate(err);
+        }else if (!user) {
+          res.status('401');
+          return res.json({
+            status: -401,
+            message: 'Invalid login!!'
+          });
+        }else if (user) {
+          User.comparePassword(password, user.password)
+          .then(() => {
+            User.checkPermission(user, loginas)
+            .then(() =>{
+              return res.json({
+                token: jwToken.issue({
+                  id: user.id,
+                  org_id: user.org_id,
+                  inst_id: user.inst_id
+                })
+              });
+            })
+            .catch((err) => {
+              res.status('401');
+              return res.json({
+                status: -401,
+                message: "You dont have permission to access this page!!"
+              });
+            });
+          })
+          .catch((err) => {
+            return res.forbidden();
+          });
+        }
+      });
+    }else {
+      res.status('400');
+      return res.json({
+        status: -400,
+        message: "Details are not provided!!"
+      });
+    }
+
+  }
+
+};
--- sms/api/models/User.js
+++ sms/api/models/User.js
@@ -4,45 +4,122 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
-'use strict';
-const bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt');
+
 
 module.exports = {
+  ORG_ADMIN: 1,
+  INST_ADMIN: 2,
+  PARENT: 3,
+  STUDENT:4,
+  schema: true,
+  attributes: {
+    username:{
+      type: 'string',
+      required: false
+    },
+    email:{
+      type: 'email',
+      required: true
+    },
+    mobile:{
+      type:'string',
+      required: false
+    },
+    password:{
+      type: 'string',
+      required: true
+    },
+    firstName:{
+      type: 'string',
+      required: true
+    },
+    lastName:{
+      type: 'string',
+      required: true
+    },
+    fullName: function () {
+      return this.firstName+' '+this.lastName;
+    },
+    is_org_admin:{
+      type: 'boolean',
+      required: true
+    },
+    is_inst_admin:{
+      type: 'boolean',
+      required: true
+    },
+    is_parent:{
+      type: 'boolean',
+      required: true
+    },
+    is_student:{
+      type: 'boolean',
+      required: true
+    },
+    org_id:{
+      type: 'string',
+      required: false
+    },
+    inst_id:{
+      type: 'string',
+      required: false
+    }
+  },
 
- attributes: {
-    
- },
+  beforeCreate: function(values, next) {
+    bcrypt.hash(values.password, 10, function(err, hash) {
+      if(err) return next(err);
+      values.password = hash;
+      if (values.is_org_admin && values.org_name != null){
+        Organisation.create({
+          name: values.org_name
+        }).exec(function (err, createdRecord){
+          delete values.org_name;
+          values.org_id = createdRecord.id;
+          next();
+        });
+      }else{
+        next();
+      }
+    });
+  },
+  comparePassword(password, encryptedPassword) {
 
- // Here we encrypt password before creating a User
- beforeCreate(values, next) {
-     bcrypt.genSalt(10, (err, salt) => {
-         if (err) {
-             sails.log.error(err);
-             return next();
-         }
+    return new Promise(function(resolve, reject) {
+      bcrypt.compare(password, encryptedPassword, (err, match) => {
+        if (err) {
+          sails.log.error(err);
+          return reject("Something went wrong!");
+        }
+        if (match) return resolve();
+        else return reject("Mismatch passwords");
+      });
+    });
+  },
+  checkPermission(user, permission){
+    return new Promise(function(resolve, reject) {
+      var permitted = false;
+      switch (permission) {
+        case User.ORG_ADMIN:
+          if (user.is_org_admin) permitted = true;
+          break;
+        case User.INST_ADMIN:
+          if (user.is_inst_admin) permitted = true;
+          break;
+        case User.PARENT:
+          if (user.is_parent) permitted = true;
+          break;
+        case User.STUDENT:
+          if (user.is_student) permitted = true;
+          break;
+      }
+      if (permitted){
+        return resolve();
+      }else{
+        return reject();
+      }
+    });
+  }
 
-         bcrypt.hash(values.password, salt, (err, hash) => {
-             if (err) {
-                 sails.log.error(err);
-                 return next();
-             }
-             values.encryptedPassword = hash; // Here is our encrypted password
-             return next();
-         });
-     });
- },
-
-     comparePassword(password, encryptedPassword) {
-
-         return new Promise(function(resolve, reject) {
-             bcrypt.compare(password, encryptedPassword, (err, match) => {
-                 if (err) {
-                     sails.log.error(err);
-                     return reject("Something went wrong!");
-                 }
-                 if (match) return resolve();
-                 else return reject("Mismatch passwords");
-             });
-         });
-     }
- };
+};
